639a814a193adb04c35b6f92f39773a72924bb89
Jacob Chia <chuyou.chia@gmail.com>
2022-09-19 22:49:09 -0700

    update prisma commands

--- pages/api/create-short-url.tsx
+++ pages/api/create-short-url.tsx
@@ -1,25 +1,52 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createShortUrl } from "../../utils/create-short-url";
-import { findMatchingUrl } from "../../utils/find-matching-url";
+import { generateUniqueHashFromString } from "../../utils/generate-unique-hash";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') {
-        //if it isn't a post request, return not allowed
-        res.status(405).json({ error: 'METHOD NOT ALLOWED' })
+export const getUrlObjectFromUserInput = async (url: string) => {
+    return await prisma.urls.findUnique({
+      where: {
+        url: url,
+      }
+    });
+  }
+  
+  export const createUrlObject = async (url: string) => {
+    // keep retrying prisma create until hash is unique
+    while (true) {
+      try {
+        let hash = generateUniqueHashFromString(url);
+        return await prisma.urls.create({
+          data: {
+            url: url,
+            hash: hash,
+          }
+        });
+      } catch (error) {
+        // do nothing
+      }
     }
+  }
+  
+  export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // guard clause against non post requests
+    if (req.method !== "POST") {
+      return res.status(405).json({ message: "Method Not Allowed" });
+    }
+  
+    // Get data from request
     const data = req.body;
     const { url } = data;
-
-    // check if the original url exists as an entry in the DB
-    const existingUrl = await findMatchingUrl(url);
-
-    if (!existingUrl){
-        const createdHash = await createShortUrl(url);
-        //return that shortened url if created        
-        return res.status(200).json({ url: createdHash.hash })
-    } else {
-        // return the originally shortened url 
-        return res.status(200).json({ url: existingUrl.hash })
+  
+    // check if url already exists in db
+    const existingUrlObject = await getUrlObjectFromUserInput(url);
+  
+    let returnedUrlObject = existingUrlObject;
+  
+    // if url doesn't exist in db, create unique hash and insert into db
+    if (!existingUrlObject) {
+      returnedUrlObject = await createUrlObject(url);
     }
-
-}
+  
+    return res.status(200).json({
+      shortUrlHash: returnedUrlObject?.hash,
+    });
+  }
