96d86d54f2b28c7fa0638ff226b45c23ef623482
Jacob Chia <chuyou.chia@gmail.com>
2022-09-21 10:09:12 -0700

    sentence case

--- tests/use-url-form.test.ts
+++ tests/use-url-form.test.ts
@@ -22,9 +22,11 @@ test('invalid url will not make api requests', async () => {
   });
 
   test('valid url will make api requests', async () => {
+    const urlToShorten = 
+    "https://stackoverflow.com/questions/45168803/jestjs-trying-to-mock-async-await-in-node-js-tests";
     const mockApiRequest = jest.fn();
     mockApiRequest.mockResolvedValue({
-        url: 'https://stackoverflow.com/questions/45168803/jestjs-trying-to-mock-async-await-in-node-js-tests', 
+        url: urlToShorten, 
         hash: '123456'
     });
 
@@ -35,14 +37,14 @@ test('invalid url will not make api requests', async () => {
     );
   
     act(() => {
-      hook.result.current.setUrlToBeShortened('https://stackoverflow.com/questions/45168803/jestjs-trying-to-mock-async-await-in-node-js-tests');
+      hook.result.current.setUrlToBeShortened(urlToShorten);
     });
     
     await act(async () => {
         await hook.result.current.onSubmitUrl();
       });
 
-    expect(hook.result.current.urlToBeShortened).toEqual('https://stackoverflow.com/questions/45168803/jestjs-trying-to-mock-async-await-in-node-js-tests');
-    expect(hook.result.current.shortenedUrl).toEqual('123456');
+    expect(hook.result.current.urlToBeShortened).toEqual(urlToShorten);
+    expect(hook.result.current.shortenedUrl).toEqual('http://localhost/123456');
     expect(mockApiRequest).toHaveBeenCalledTimes(1);
   });
